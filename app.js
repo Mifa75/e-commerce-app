0c6fa2c12202a8b4d909d46711c2ce3b2e700d6e
Mifa75 <scanosilvia75@gmail.com>
2024-12-26 16:54:39 +0000

    Modified app.js file

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const userRoutes = require('./routes/users');
-
 const app = express();
+const PORT = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
 
+// Define a root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
+
 // User routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
